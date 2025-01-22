5b43cd31cc0c09516cfb2ff45d9373861331f4cd
Aydamohamadi <aydammohammadiii@gmail.com>
2025-01-22 14:04:26 -0800

    firsttamrin

---
+++ app/Http/Controllers/OstanController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Ostan;
+
+use Illuminate\Http\Request;
+
+class OstanController extends Controller
+{
+    public function getOstan(){
+     $result= Ostan::all();
+     return response()->json(['result' => $result], 200);
+
+    }
+}
---
+++ app/Models/Ostan.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Ostan extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'name',
+    ];
+}
